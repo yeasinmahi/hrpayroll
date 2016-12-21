1ae04102eafec3e8cf137934fab1b06a6a332c2e
yeasinmahi <yeasinmahi72@yahoo.com>
2016-12-21 15:36:04 +0600

    first project start

---
+++ HrPayroll/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HrPayroll
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HrPayroll/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HrPayroll.MvcApplication" Language="C#" %>
