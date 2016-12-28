65cfc2d5eecf9cfab6f64a4042bdc6c06f2100ec
yeasinmahi <yeasinmahi72@yahoo.com>
2016-12-28 10:39:30 +0600

    adding db and meta data class

---
+++ HrPayroll/Models/MetaData.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HrPayroll.Models
+{
+    [MetadataType(typeof(EmployeeMeta))]
+    public partial class Employee
+    {
+
+    }
+
+    public class EmployeeMeta
+    {
+        
+    }
+}
